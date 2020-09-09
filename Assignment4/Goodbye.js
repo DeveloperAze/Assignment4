2f2f91573a39fa8394d495529b0e72043459d2fa
DeveloperAze <68186542+DeveloperAze@users.noreply.github.com>
2020-09-09 15:22:55 +0400

    Update Goodbye.js

--- Assignment4/Goodbye.js
+++ Assignment4/Goodbye.js
@@ -19,7 +19,7 @@ byeSpeaker.speakWord = "Good Bye";
 // See Lecture 52, part 2
 byeSpeaker.speak=( function (window.name) {
   console.log(byeSpeaker.speakWord + " " + name);
-})(window);
+})(window.name);
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
