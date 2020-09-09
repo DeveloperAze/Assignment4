b4518eed8faf0781d499f5a7b02c3f7cc373a39d
DeveloperAze <68186542+DeveloperAze@users.noreply.github.com>
2020-09-09 16:15:25 +0400

    Update Goodbye.js

--- Assignment4/Goodbye.js
+++ Assignment4/Goodbye.js
@@ -8,11 +8,12 @@
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-// var byeSpeaker = 
-
-// DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+// var byeSpeaker =
 (function (){var byeSpeaker={};
 byeSpeaker.speakWord = "Good Bye";}
+
+// DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
