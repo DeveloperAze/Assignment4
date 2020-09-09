e5b333733cedf3e76b17ebdd69931d62d4690562
DeveloperAze <68186542+DeveloperAze@users.noreply.github.com>
2020-09-09 16:38:59 +0400

    Update Goodbye.js

--- Assignment4/Goodbye.js
+++ Assignment4/Goodbye.js
@@ -9,7 +9,7 @@
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 // var byeSpeaker =
-function(window){
+(function(window){
 var byeSpeaker={};
 byeSpeaker.speakWord = "Good Bye";
 
